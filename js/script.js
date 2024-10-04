9a0b4507ce18b19ee15ea55e2fd8af66dee8933a
bfaturrochman <fabagasz@gmail.com>
2024-10-05 00:56:11 +0700

    add fungsi konversi dan reset

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// jScript
+function tombolKonversi() {
+    let input = document.getElementById("input").value;
+    let hasil = input * (9 / 5) + 32;
+    document.getElementById("hasil1").value = hasil;
+    document.getElementById("hasil2").value = input, "°C * (9/5) + 32 = ", hasil;
+}
+
+function tombolReset() {
+    document.getElementById("input").value = "";
+    document.getElementById("hasil1").value = "";
+    document.getElementById("hasil2").value = "";
+}
+// fahrenheitcelcius = () =>{
+//         let input = document.getElementById('input').value;
+//         let hasil = (input-32) / (9/5);
+//         document.getElementById('hasil').value = hasil;
+//     }
